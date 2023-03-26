23442b91b879f9dd3a945b1faa378ca6f02f7b98
katernet <37143923+katernet@users.noreply.github.com>
2023-03-26 22:35:09 +1030

    Added linux style key maps Added pgup/down forward/backward-word Added push-line Changed fuzzy fns from fzy to fzf Modified clear_screen to prevent transient new line Modified scroll_pgup to query cursor position

--- .config/zsh/keybinds.zsh
+++ .config/zsh/keybinds.zsh
@@ -1,17 +1,86 @@
-# Keybinds
-autoload -Uz up-line-or-beginning-search down-line-or-beginning-search
-bindkey '^[[3~'	delete-char			# [Del]- Forward delete
-bindkey '^[[H'	beginning-of-line		# [Home]- Beginning of line
-bindkey '^[[F'	end-of-line			# [End]- End of line
-bindkey '^[[A'	up-line-or-beginning-search	# [Up]- History search up
-bindkey '^[[B'	down-line-or-beginning-search	# [Down]- History search down
+[[ "$TERM" != 'emacs' ]] || return
 
-# History search with arrow keys
+# Create a keybind hash
+# Adapted from Debian system zshrc and https://wiki.archlinux.org/title/zsh#Key_bindings
+typeset -A key
+key=(
+	BackSpace  "${terminfo[kbs]}"
+	Home       "${terminfo[khome]}"
+	End        "${terminfo[kend]}"
+	Insert     "${terminfo[kich1]}"
+	Delete     "${terminfo[kdch1]}"
+	Up         "${terminfo[kcuu1]}"
+	Down       "${terminfo[kcud1]}"
+	Left       "${terminfo[kcub1]}"
+	Right      "${terminfo[kcuf1]}"
+	PageUp     "${terminfo[kpp]}"
+	PageDown   "${terminfo[knp]}"
+	Shift-Tab  "${terminfo[kcbt]}"
+)
+
+bindkeymap() {
+	local i sequence widget
+	local -a maps
+	while [[ "$1" != "--" ]]; do
+		maps+=( "$1" )
+		shift
+	done
+	shift
+	sequence="${key[$1]}"
+	widget="$2"
+	[[ -z "$sequence" ]] && return 1
+	for i in "${maps[@]}"; do
+		bindkey -M "$i" "$sequence" "$widget"
+	done
+}
+
+bindkeymap emacs             -- BackSpace   backward-delete-char
+bindkeymap       viins       -- BackSpace   vi-backward-delete-char
+bindkeymap             vicmd -- BackSpace   vi-backward-char
+bindkeymap emacs             -- Home        beginning-of-line
+bindkeymap       viins vicmd -- Home        vi-beginning-of-line
+bindkeymap emacs             -- End         end-of-line
+bindkeymap       viins vicmd -- End         vi-end-of-line
+bindkeymap emacs viins       -- Insert      overwrite-mode
+bindkeymap             vicmd -- Insert      vi-insert
+bindkeymap emacs             -- Delete      delete-char
+bindkeymap       viins vicmd -- Delete      vi-delete-char
+bindkeymap emacs viins vicmd -- Up          up-line-or-history
+bindkeymap emacs viins vicmd -- Down        down-line-or-history
+bindkeymap emacs             -- Left        backward-char
+bindkeymap       viins vicmd -- Left        vi-backward-char
+bindkeymap emacs             -- Right       forward-char
+bindkeymap       viins vicmd -- Right       vi-forward-char
+bindkeymap emacs viins       -- Shift-Tab   reverse-menu-complete
+bindkeymap emacs viins       -- PageUp	    forward-word
+bindkeymap emacs viins       -- PageDown    backward-word
+bindkeymap emacs viins       -- -s '^q'     push-line
+
+# Make sure the terminal is in application mode when zle is active
+# Only then are the values from $terminfo valid
+if (( ${+terminfo[smkx]} && ${+terminfo[rmkx]} )); then
+	key_start() {
+		emulate -L zsh
+		printf '%s' ${terminfo[smkx]}
+	}
+	key_end() {
+		emulate -L zsh
+		printf '%s' ${terminfo[rmkx]}
+	}
+fi
+
+unfunction bindkeymap
+
+# History search keybinds
+autoload -Uz up-line-or-beginning-search down-line-or-beginning-search
 zle -N up-line-or-beginning-search
 zle -N down-line-or-beginning-search
 
+[[ -n "${key[Up]}"   ]] && bindkey -- "${key[Up]}"   up-line-or-beginning-search
+[[ -n "${key[Down]}" ]] && bindkey -- "${key[Down]}" down-line-or-beginning-search
+
 # Keybind functions
-# For setting alt in macOS iTerm2 see https://www.iterm2.com/faq.html
+# Mac users: for using alt in Term2 see https://www.iterm2.com/faq.html
 
 # [Alt+L] - List files in an empty buffer, else use builtin down-case-word widget.
 list_files() {
@@ -26,18 +95,24 @@ zle -N list_files
 bindkey '^[l' list_files
 
 # [Alt+D] - Show dir stack below prompt
-typeset -g dirmsg
 show_dirs() {
-	typeset -g dm=y
-	POSTDISPLAY=$'\n'"${$(d)//$'\t'/       }"
+	dmsg "${$(d)//$'\t'/       }"
 }
 zle -N show_dirs
 bindkey '^[d' show_dirs
 
-# [Alt+Enter] - Run fuzzy cd fpath function
+# [Alt+G] - Run fuzzy dir stack cd function
+fuzzy_dirs() {
+	zle -I
+	fd
+}
+zle -N fuzzy_dirs
+bindkey '^[g' fuzzy_dirs
+
+# [Alt+Enter] - Run fuzzy dir stack fpath function
 fuzzy_cd() {
 	zle -I
-	fcd
+	fd
 }
 zle -N fuzzy_cd
 bindkey '^[^M' fuzzy_cd
@@ -46,41 +121,48 @@ bindkey '^[^M' fuzzy_cd
 fuzzy_editor() {
 	zle -I
 	fe
+	if [ "$RESULT" ]; then
+		LBUFFER="$EDITOR $RESULT"
+		builtin zle accept-line
+		unset RESULT
+	fi
 }
 zle -N fuzzy_editor
 bindkey '^[s' fuzzy_editor
 
 # [Alt+R] - Fuzzy history search
 fuzzy_hist() {
-	(($+commands[fzy])) || { echo "Function requires: fzy" >&2 && return 1 }
+	(($+commands[fzf])) || { echo "Function requires: fzf" >&2 && return 1 }
 	local tac
 	[[ "$OSTYPE" == darwin* ]] && tac='tail -r' || tac='tac'
-	BUFFER=$(history -n 1 | eval $tac | fzy)
+	BUFFER=$(history -n 1 | eval $tac | fzf)
 	CURSOR=$#BUFFER
 	builtin zle reset-prompt
 }
 zle -N fuzzy_hist
 bindkey '^[r' fuzzy_hist
 
-# [Ctrl+L] - Clear screen and unset prompt vars
+# [Ctrl+L] - Clear screen replacement to unset prompt vars
 clear_screen() {
-	[ -n "$paste" ] 	&& unset paste
-	[ -n "$exitcode" ] 	&& unset exitcode
-	[ -n "$timer_result" ]  && unset timer_result
-	[ -n "$__searching" ]   && unset __searching
-	[ -n "$nocmdtime" ] 	&& unset nocmdtime
-	[ -n "$histline" ]	&& unset histline
-	[ -n "$dirmsg" ] 	&& unset dirmsg
-	[ -n "$dm" ] 		&& unset dm
-	builtin zle clear-screen
+	[ "$paste" ]	    	&& unset paste
+	[ "$exitcode" ]	    	&& unset exitcode
+	[ "$timer_result" ]	&& unset timer_result
+	[ "$__searching" ]	&& unset __searching
+	[ "$nocmdtime" ]	&& unset nocmdtime
+	[ "$histline" ]		&& unset histline
+	[ "$dirmsg" ]		&& unset dirmsg
+	[ "$dm" ]		&& unset dm
+	# Redraw prompt to prevent transient new line
+	[[ "$PROMPT_TRANSIENTOPT" =~ 'newline' ]] && prompt_setup "$@"
+	zle clear-screen
 }
 zle -N clear_screen
 bindkey '^L' clear_screen
 
 # [Ctrl+U] - Kill line and unset prompt vars
 kill_line() {
-	[ -n "$paste" ] 	&& unset paste
-	[ -n "$__searching" ]   && unset __searching
+	[ "$paste" ] && unset paste
+	[ "$__searching" ] && unset __searching
 	builtin zle kill-whole-line
 }
 zle -N kill_line
@@ -99,20 +181,18 @@ fancy_ctrlz() {
 zle -N fancy_ctrlz
 bindkey '^Z' fancy_ctrlz
 
-# [Ctrl+X] - Prepend sudo to a command
+# [Ctrl+X] - Prepend sudo to prompt
 prepend_sudo() {
-  if [[ $BUFFER != "sudo "* ]]; then
-      BUFFER="sudo $BUFFER"
-      CURSOR+=5
-  fi
+	if [[ $BUFFER != "sudo "* ]]; then
+		BUFFER="sudo $BUFFER"
+		CURSOR+=5
+	fi
 }
 zle -N prepend_sudo
 bindkey '^X' prepend_sudo
 
 # [Ctrl+X,E] - Edit prompt in text editor
 edit_cmdline() {
-	# Set VISUAL to your preferred editor
-	(($+commands[micro])) && local VISUAL='micro' || local VISUAL='vim'
 	autoload edit-command-line
 	edit-command-line
 }
@@ -121,12 +201,19 @@ bindkey '^X^E' edit_cmdline
 
 # [Ctrl+K] - Pgup half a page and keep scrollback
 scroll_pgup() {
-	local page
-	((page=$LINES/2)) # Half of Terminal lines
-	for i in {1..$page}; do
+	local pg pos
+	# Read position of cursor. Adapted from https://stackoverflow.com/a/43911767/5798232
+	echo -ne '\e[6n' > /dev/tty	    # Ask the terminal for the position
+	read -t 1 -s -r -d\[ pos < /dev/tty # Garbage
+	read -t 1 -s -r -d R pos < /dev/tty # Cursor position
+	pos=${pos//;*}
+	((pg=$LINES/2)) 	  # Half of Terminal lines
+	(($pos <= $pg)) && return # Cursor position is already half a page
+	for i in {1..$pg}; do
 		print ''
 	done
-	tput cup $(($page-1)) # First line position is 0
+	tput cup $(($pg-1)) # Set the cursor position to half a page
+	builtin zle reset-prompt
 }
 zle -N scroll_pgup
 bindkey '^K' scroll_pgup
